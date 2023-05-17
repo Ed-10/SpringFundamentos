a89747825fd8c3e96c8117b07f672a7b09a0be28
Eduardo Ramirez <72027500+Ed-10@users.noreply.github.com>
2023-05-17 17:26:30 -0600

    Update Constantes.java

--- src/main/java/cl/tamila/utilidades/Constantes.java
+++ src/main/java/cl/tamila/utilidades/Constantes.java
@@ -1,10 +1,10 @@
 package cl.tamila.utilidades;
 
 public class Constantes     {
-    public static final String SMTP="af356cf585bd8c";
-    public static final String SMTP_MAIL="sandbox.smtp.mailtrap.io";
-    public static final String SMTP_PASSWORD="50f49e81b5c32d";
-    public static final String SMTP_PORT="2525";
+    public static final String SMTP="";
+    public static final String SMTP_MAIL="";
+    public static final String SMTP_PASSWORD="";
+    public static final String SMTP_PORT="";
 
     /*
     public final String getSMTP () {
